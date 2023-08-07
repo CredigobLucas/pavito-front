dd6283a4e1a472ef859627b05845157d6a163b2c
Lucas Moreno <lucas.moreno@credigob.pe>
2023-08-07 15:53:11 -0500

    a

--- src/app/pavito/filter/component/DataGrid.tsx
+++ src/app/pavito/filter/component/DataGrid.tsx
@@ -1,14 +1,22 @@
 "use client";
 import { Bid } from "@/domain/models";
+import { Grid, Paper } from "@mui/material";
 interface DataGridProps {
     bids: Bid[];
 }
 
 export const DataGrid = ({ bids }: DataGridProps): JSX.Element => {
     return (
-        <pre>
-            grilla
-            <code>{JSON.stringify(bids, null, 2)}</code>
-        </pre>
+        <Grid container>
+            {bids.map((bid, index) => (
+                <Grid className="p-2" item key={index} xs={12} md={6}>
+                    <Paper elevation={3}>
+                        <pre className="p-4 whitespace-pre-wrap">
+                            {JSON.stringify(bid, null, 2)}
+                        </pre>
+                    </Paper>
+                </Grid>
+            ))}
+        </Grid>
     );
 };
