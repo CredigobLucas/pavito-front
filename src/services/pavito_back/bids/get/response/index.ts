36edd9a11a8da37e26a0a35314b66c37fb4c2a86
DiegoOrtizS <diego.ortiz@utec.edu.pe>
2023-08-16 01:51:48 -0500

    hotfix: typo on GetBidResponse

--- src/services/pavito_back/bids/get/response/index.ts
+++ src/services/pavito_back/bids/get/response/index.ts
@@ -1,7 +1,7 @@
 import { Bid } from "@/domain/models";
 import { GenericStatusResponse } from "@/services/pavito_back/generic/response";
 
-export interface GetBidRespons extends GenericStatusResponse {
+export interface GetBidResponse extends GenericStatusResponse {
     body: {
         paginaVigente: number;
         numeroPaginas: number;
