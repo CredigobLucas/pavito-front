71e6b9be8d7a9ad827d067cf22f5a0481448e108
Lucas Moreno <lucas.moreno@credigob.pe>
2023-08-22 15:54:13 -0500

    security ok

---
+++ src/app/management/admin/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useLayoutEffect, useState } from "react";
+import { useGlobalContext } from "../../context";
+import { useRouter } from "next/navigation";
+import { Box } from "@mui/material";
+
+export default function ManagmentLayout({
+    children
+}: {
+    children: React.ReactNode;
+}): JSX.Element {
+    const { user, openAlertMessage } = useGlobalContext();
+    const router = useRouter();
+    const [authorized, setAuthorized] = useState<boolean>(false);
+    useLayoutEffect(() => {
+        if (user?.id) {
+            if (user.is_admin) {
+                setAuthorized(true);
+            } else {
+                openAlertMessage({
+                    horizontal: "center",
+                    vertical: "top",
+                    severity: "error",
+                    message: "No tienes permisos para acceder a esta página"
+                });
+                router.push("/");
+            }
+        }
+    }, [user]);
+    return <>{authorized ? <>{children}</> : <></>}</>;
+}
