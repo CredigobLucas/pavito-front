0d52d7f28797e2897792cc1cfc9f09e8bf3ece00
Lucas Moreno <lucas.moreno@credigob.pe>
2023-08-22 15:27:15 -0500

    if not admin ok

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useGlobalContext } from "../context";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { Box } from "@mui/material";
 import { Module } from "@/domain/interface/Module";
 import { Settings, Person, DataObject } from "@mui/icons-material";
@@ -8,34 +8,40 @@ import { Settings, Person, DataObject } from "@mui/icons-material";
 import { UserBanner, ModuleCard } from "./components";
 import { accountOptions, adminOptions } from "./options";
 
-
 export default function Dashboard(): JSX.Element {
     const { setSectionTitle, user } = useGlobalContext();
-    const modules: Module[] = [
-        {
-            title: "Administración",
-            description: "Administra los usuarios y verifica tu licencia",
-            icon: <Settings htmlColor="#0369A1" />,
-            backgroundColor: "#BAE6FD",
-            link: "/management/admin/accounts",
-            menuOptions: adminOptions
-        },
-        {
-            title: "Cuenta",
-            description: "Administra tu cuenta y tus datos personales",
-            icon: <Person htmlColor="#047857" />,
-            backgroundColor: "#D1FAE5",
-            link: "/management/account/user",
-            menuOptions: accountOptions
-        },
-        {
-            title: "Pavito",
-            description: "Busqueda de prospectos de calidad",
-            icon: <DataObject htmlColor="#62569A" />,
-            backgroundColor: "#DDD6FE",
-            link: "/pavito/filter"
+    const modules = useMemo<Module[]>(() => {
+        const m = [
+            {
+                title: "Administración",
+                description: "Administra los usuarios y verifica tu licencia",
+                icon: <Settings htmlColor="#0369A1" />,
+                backgroundColor: "#BAE6FD",
+                link: "/management/admin/accounts",
+                menuOptions: adminOptions
+            },
+            {
+                title: "Cuenta",
+                description: "Administra tu cuenta y tus datos personales",
+                icon: <Person htmlColor="#047857" />,
+                backgroundColor: "#D1FAE5",
+                link: "/management/account/user",
+                menuOptions: accountOptions
+            },
+            {
+                title: "Pavito",
+                description: "Busqueda de prospectos de calidad",
+                icon: <DataObject htmlColor="#62569A" />,
+                backgroundColor: "#DDD6FE",
+                link: "/pavito/filter"
+            }
+        ];
+        if (!user?.is_admin) {
+            m.splice(0, 1);
         }
-    ];
+        return m;
+    }, [user]);
+
     useEffect(() => {
         setSectionTitle("logo");
     }, []);
