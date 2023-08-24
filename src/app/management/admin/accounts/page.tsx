ef760f9da5bfbec9f061e29519192bf4db20e034
DiegoOrtizS <diego.ortiz@utec.edu.pe>
2023-08-24 01:53:39 -0500

    feat: change toogle to checkbox and label to be static in solo activos

--- src/app/management/admin/accounts/page.tsx
+++ src/app/management/admin/accounts/page.tsx
@@ -9,6 +9,7 @@ import {
     Chip,
     Select,
     Switch,
+    Checkbox,
     Menu,
     MenuItem,
     IconButton,
@@ -241,15 +242,14 @@ export default function Admin(): JSX.Element {
                 <FormControlLabel
                     className="mr-5"
                     control={
-                        <Switch
-                            size="small"
+                        <Checkbox
                             checked={onlyActive}
                             onChange={(): void => {
                                 setOnlyActive(!onlyActive);
                             }}
                         />
                     }
-                    label={onlyActive ? "Todos" : "Solo activos"}
+                    label={"Solo activos"}
                 />
             </Box>
             <PavitoTable<User>
